72a38cbac053c80ff8b00656164e635d2e6f6b2e
Zikra Daffa Saputra <zdsaputra@gmail.com>
2026-08-26 22:09:26 +0700

    implement footer with email validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HeroSection from './components/sections/HeroSection'
 import FeatureSection from './components/sections/FeatureSection'
 import TestimonialsSection from './components/sections/TestimonialsSection'
 import CtaSection from './components/sections/CtaSection'
+import Footer from './components/layout/Footer'
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 				<TestimonialsSection />
 				<CtaSection />
 			</main>
+			<Footer />
 		</>
 	)
 }
